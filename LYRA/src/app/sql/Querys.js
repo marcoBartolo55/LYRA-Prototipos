ce57d4dd0487d0ae0b44d1e5540a26471a97adbe
BigSusFG <118015087+BigSusFG@users.noreply.github.com>
2023-05-15 23:42:35 -0600

    Querys Inicio de sesión

--- LYRA/src/app/sql/Querys.js
+++ LYRA/src/app/sql/Querys.js
@@ -1,2 +1,15 @@
 const querys = require('../sql/Querys');
 const Controllers={};
+
+db.buscarUsuario = (nombre_usuario) => {
+  return new Promise((resolve, reject) => {
+    con.query(`SELECT * FROM usuario NATURAL JOIN persona where nombre_usuario = ?`, [nombre_usuario], (error, result) => {
+      if (error) {
+        console.error(error);
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+};
