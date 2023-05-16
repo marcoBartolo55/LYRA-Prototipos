e83892507b5e452bd78863196fc86f5bda9d1b80
BigSusFG <118015087+BigSusFG@users.noreply.github.com>
2023-05-15 23:51:31 -0600

    Querys Ver conversaciones

--- LYRA/src/app/sql/Querys.js
+++ LYRA/src/app/sql/Querys.js
@@ -1,6 +1,20 @@
 const querys = require('../sql/Querys');
 const Controllers={};
 
+db.InfoPacientes = (Paciente)=>{
+  return new Promise(async(resolve, reject) =>{
+    const query = `SELECT * FROM datospaciente where nombre_usuario = '${Paciente}'`;
+    con.query(query,(error,result)=>{
+      if (error) {
+        console.error(error);
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+  })
+};
+
 db.buscarUsuario = (nombre_usuario) => {
   return new Promise((resolve, reject) => {
     con.query(`SELECT * FROM usuario NATURAL JOIN persona where nombre_usuario = ?`, [nombre_usuario], (error, result) => {
