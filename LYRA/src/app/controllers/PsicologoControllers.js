5d60fb29fab25176a5be03e7ba51baa924fd4281
QuandaleDingle5000 <117874132+QuandaleDingle5000@users.noreply.github.com>
2023-05-16 22:15:24 -0500

    Modelo y Controlador PaginaPrincipalPacientes DesplegarDatos

--- LYRA/src/app/controllers/PsicologoControllers.js
+++ LYRA/src/app/controllers/PsicologoControllers.js
@@ -1,3 +1,16 @@
 const querys = require('../sql/Querys');
 const Controllers={};
 module.exports = Controllers;
+
+Controllers.Principal = async(req,res,next)=>{
+    const Usuario = req.session.usuario;
+    const TipoUsu = req.session.tipo_usuario;
+    const alerta = req.query.alerta;
+    try{
+    const datosUsuario =await querys.buscarUsuario(Usuario);
+    const Pacientes = await querys.DesplegarPacientes(Usuario);
+    res.render('PaginaPrincipalPsicologos',{Usuario,TipoUsu,datosUsuario,Pacientes,alerta});
+    }catch(error){
+      console.error(error);
+    }
+  };
--- LYRA/src/app/sql/Querys.js
+++ LYRA/src/app/sql/Querys.js
@@ -1,2 +1,29 @@
 const querys = require('../sql/Querys');
 const Controllers={};
+
+db.buscarUsuario = (nombre_usuario) => {
+    return new Promise((resolve, reject) => {
+      con.query(`SELECT * FROM usuario NATURAL JOIN persona where nombre_usuario = ?`, [nombre_usuario], (error, result) => {
+        if (error) {
+          console.error(error);
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  };
+
+  db.DesplegarPacientes = (Doctor)=>{
+    return new Promise(async(resolve, reject) =>{
+      const query = `SELECT * FROM vista_pacientes_doctor where nombre_Doctor = '${Doctor}'`;
+      con.query(query,(error,result)=>{
+        if (error) {
+          console.error(error);
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      });
+    })
+  };
