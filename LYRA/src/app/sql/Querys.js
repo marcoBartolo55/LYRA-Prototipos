b4d733d59756dc9d44e303edb4e9d819085207e3
3nr1qu30 <evarillasf2000@alumno.ipn.mx>
2023-05-29 00:12:38 -0600

    Modelo-Gerente-Soporte

--- LYRA/src/app/sql/Querys.js
+++ LYRA/src/app/sql/Querys.js
@@ -101,3 +101,78 @@ console.log(fechaHoraFormateada);
   })
 }
 
+db.ActualizarReporteGerentes =(Id_Reporte,UsuarioAsignado,UsuarioManipula,Estado)=>{
+  return new Promise(async(resolve, reject)=>{
+    // Obtener la fecha y hora actual en la zona horaria de México
+    const fechaHoraActual = moment().tz('America/Mexico_City');
+    // Formatear la fecha y hora en el formato deseado para MySQL (datetime: 'YYYY-MM-DD HH:MM:SS')
+    const fechaHoraFormateada = fechaHoraActual.format('YYYY-MM-DD HH:mm:ss');
+    const Estatus=parseInt(Estado) 
+    const query = `update reporte set id_usuario_manipulo_Reporte='${UsuarioManipula}', id_Repore_Estatus=${Estatus}, id_usuario_asignado='${UsuarioAsignado}', fecha_hora='${fechaHoraFormateada}' where id_reporte=${Id_Reporte} `;
+    con.query(query,(error,result)=>{
+      if (error) {
+        console.error(error);
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+}
+
+db.Actualizardatos = (Id_Usuario,Nombre,Apellido,Edad,Sexo,Correo)=>{
+  return new Promise(async(resolve, reject)=>{
+    const query = `CALL actualizar_persona_usuario(?, ?, ?, ?, ?, ?)`;
+    const values = [Id_Usuario,Nombre,Apellido,Edad,Sexo,Correo];
+    con.query(query, values, (error,result)=>{
+      if (error) {
+        console.error(error);
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+};
+
+//Enrique
+db.Reportes=()=>{
+  return new Promise(async(resolve, reject) =>{
+    const query = `SELECT r.*, e.descripcion_estatus FROM Reporte r JOIN Reporte_Estatus e ON r.id_Repore_Estatus = e.id_Reporte_Estatus;`;
+    con.query(query,(error,result)=>{
+      if (error) {
+        console.error(error);
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+  })
+};
+
+//Enrique
+db.buscarGerentesSoporte = () =>{
+  return new Promise((resolve, reject) => {
+    con.query(`SELECT * FROM usuario NATURAL JOIN persona where id_tipo_usuario = 3`, (error, result) => {
+      if (error) {
+        console.error(error);
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+}
+//Enrique
+db.buscarDoctoresyPacientes = () =>{
+  return new Promise((resolve, reject) => {
+    con.query(`SELECT * FROM usuario NATURAL JOIN persona where id_tipo_usuario = 1 or id_tipo_usuario = 2`, (error, result) => {
+      if (error) {
+        console.error(error);
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+}
